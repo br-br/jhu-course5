e57c10c68485bc45aac4a37295fe95bca96a950e
britte breindl <bribre111@gmail.com>
2017-02-10 12:39:03 +0100

    First commit

---
+++ assignment4-solution/src/services/menudata.service.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular.module('data')
+  .service('MenuDataService', MenuDataService);
+
+  // returns a promise which is a result of using the $http service
+  MenuDataService.$inject = ['$http', 'ApiBasePath'];
+  function MenuDataService($http, ApiBasePath) {
+    var service = this;
+
+    service.getAllCategories = function() {
+        return $http({
+            method: "GET",
+            url: (ApiBasePath + '/categories.json')
+        }).then(function(response) {;
+            return response.data;
+        });
+    };
+
+    // returns a promise which is a result of using the $http service
+    service.getItemsForCategory = function(categoryShortName) {
+        return $http({
+            method: "GET",
+            url: (ApiBasePath + '/menu_items.json?category=' + categoryShortName)
+        }).then(function(response) {
+            return response.data.menu_items;
+        });
+    };
+  }
+})();
