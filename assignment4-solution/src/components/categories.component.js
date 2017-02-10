2b30c3e62fe8fd624717204b210c423298444f24
britte breindl <bribre111@gmail.com>
2017-02-10 23:00:25 +0100

    categories.template changed

--- assignment4-solution/src/components/categories.component.js
+++ assignment4-solution/src/components/categories.component.js
@@ -3,7 +3,7 @@
 
     angular.module('MenuApp')
         .component('categories', {
-           templateUrl: 'src/templates/categories.component.html',
+            templateUrl: 'src/templates/categories.component.html',
             bindings: {
                 categories: '<'
             }
