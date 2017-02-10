00fcbc2d6f594c79bf1e9277c318ed2ff92dae5a
britte breindl <bribre111@gmail.com>
2017-02-10 23:20:17 +0100

    menuapp: http → https

--- assignment4-solution/src/modules/menuapp.module.js
+++ assignment4-solution/src/modules/menuapp.module.js
@@ -2,6 +2,6 @@
 'use strict';
 
 angular.module('MenuApp', ['ui.router', 'data'])
-.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 })();
