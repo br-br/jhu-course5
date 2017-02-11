4da7f6a5654850031b864bddc7d2f7d157245642
br-br <bribre27@gmail.com>
2017-02-11 15:23:40 +0100

    Update menudata.service.js

--- assignment4-solution/src/menudata.service.js
+++ assignment4-solution/src/menudata.service.js
@@ -2,7 +2,8 @@
   'use strict';
 
   angular.module('Data')
-  .service('MenuDataService', MenuDataService);
+  .service('MenuDataService', MenuDataService)
+  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
   MenuDataService.$inject = ['$http'];
   function MenuDataService($http) {
@@ -11,7 +12,7 @@
     service.getAllCategories = function () {
       return $http({
         method: "GET",
-        url: "https://davids-restaurant.herokuapp.com/categories.json"
+        url:(ApiBasePath + "/categories.json")
       }).then(function (response) {
         // console.log(response.data);
         return response.data;
@@ -21,7 +22,7 @@
     service.getItemsForCategory = function (categoryShortName) {
       return $http({
         method: "GET",
-        url: ('https://davids-restaurant.herokuapp.com/menu_items.json?category=' + categoryShortName)
+        url: ((ApiBasePath + '/menu_items.json?category=' + categoryShortName)
       }).then(function (response) {
         // console.log("Item for category ", categoryShortName);
         // console.log(response.data.menu_items);
