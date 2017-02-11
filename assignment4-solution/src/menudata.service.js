0e679e363c782645a16afbb31d49058ef04468c8
britte breindl <bribre111@gmail.com>
2017-02-11 16:29:01 +0100

    spinner added

--- assignment4-solution/src/menudata.service.js
+++ assignment4-solution/src/menudata.service.js
@@ -1,32 +1,69 @@
+// (function () {
+//   'use strict';
+//
+//   angular.module('Data')
+//   .service('MenuDataService', MenuDataService)
+//   .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com');
+//
+//   MenuDataService.$inject = ['$http', 'ApiBasePath'];
+//   function MenuDataService($http) {
+//     var service = this;
+//
+//     service.getAllCategories = function () {
+//       return $http({
+//         method: "GET",
+//         url: (ApiBasePath + '/categories.json')
+//       }).then(function (response) {
+//         // console.log(response.data);
+//         return response.data;
+//       });
+//     }
+//
+//     service.getItemsForCategory = function (categoryShortName) {
+//       return $http({
+//         method: "GET",
+//         url: (ApiBasePath + '/menu_items.json?category=' + categoryShortName)
+//       }).then(function (response) {
+//         // console.log("Item for category ", categoryShortName);
+//         // console.log(response.data.menu_items);
+//         return response.data.menu_items;
+//       })
+//     }
+//   }
+// })();
+
+
 (function () {
-  'use strict';
+ 'use strict';
 
-  angular.module('Data')
-  .service('MenuDataService', MenuDataService);
+ angular.module('Data')
+ .service('MenuDataService', MenuDataService)
+ .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
-  MenuDataService.$inject = ['$http'];
-  function MenuDataService($http) {
-    var service = this;
+ MenuDataService.$inject = ['$http', 'ApiBasePath'];
+ function MenuDataService($http, ApiBasePath) {
+   var service = this;
 
-    service.getAllCategories = function () {
-      return $http({
-        method: "GET",
-        url: "https://davids-restaurant.herokuapp.com/categories.json"
-      }).then(function (response) {
-        // console.log(response.data);
-        return response.data;
-      });
-    }
+   service.getAllCategories = function () {
+     return $http({
+       method: "GET",
+       url:(ApiBasePath + "/categories.json")
 
-    service.getItemsForCategory = function (categoryShortName) {
-      return $http({
-        method: "GET",
-        url: ('https://davids-restaurant.herokuapp.com/menu_items.json?category=' + categoryShortName)
-      }).then(function (response) {
-        // console.log("Item for category ", categoryShortName);
-        // console.log(response.data.menu_items);
-        return response.data.menu_items;
-      })
-    }
-  }
+     }).then(function (response) {
+       // console.log(response.data);
+       return response.data;
+     });
+   }
+
+   service.getItemsForCategory = function (categoryShortName) {
+     return $http({
+       method: "GET",
+       url: (ApiBasePath + '/menu_items.json?category=' + categoryShortName)
+     }).then(function (response) {
+       // console.log("Item for category ", categoryShortName);
+       // console.log(response.data.menu_items);
+       return response.data.menu_items;
+     })
+   }
+ }
 })();
--- assignment4-solution/src/spinner/spinner.module.js
+++ assignment4-solution/src/spinner/spinner.module.js
@@ -3,12 +3,12 @@
 
 angular.module('Spinner', []);
 
-angular.module('Spinner')
-.config(function () {
-  console.log("Spinner config fired.");
-}).
-run(function () {
-  console.log("Spinner run fired.");
-});
+// angular.module('Spinner')
+// .config(function () {
+//   console.log("Spinner config fired.");
+// }).
+// run(function () {
+//   console.log("Spinner run fired.");
+// });
 
 })();
